5426e73c382fff5f208fa5c5841a31911c678d5f
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-01 11:17:41 -0500

    check Lint

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const unusedVar = "hello world";
   return (
     <div className="App">
       <header className="App-header">
